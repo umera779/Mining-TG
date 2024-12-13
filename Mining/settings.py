a27b5551e6598486ab65a33346d6fa4313ab4519
umera779 <emmanuelumera107@gmail.com>
2024-12-13 11:15:55 +0100

    opps11

--- Mining/settings.py
+++ Mining/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://mining-tg-1.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
